4a45a274da4540b034dc864e30ed315c74393614
Alfred Tuva <muindetuva@gmail.com>
2023-04-01 13:40:39 +0000

    Use arrow function

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-    return { status: 200, body: 'success' };
-  })
-    .catch(() => {
+  promise
+    .then(() => (
+      { status: 200, body: 'success' }
+    ))
+    .catch(() => Error())
+    .then(() => {
       console.log('Got a response from the API');
-      return Error();
     });
 }
