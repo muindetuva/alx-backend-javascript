f2f9e2d2d5d393ca9cd64a5ea9acec444dfc451e
Alfred Tuva <muindetuva@gmail.com>
2025-05-14 11:17:50 +0300

    make eslint compliant

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,11 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) =>
-      results.map((res) =>
-        res.status === 'fulfilled'
-        ? {status: res.status, value: res.value} 
-        : {status: res.status, value: res.reason.toString() }
-      )
-    );
+    .then((results) => results.map((res) => {
+      if (res.status === 'fulfilled') {
+        return { status: res.status, value: res.value };
+      }
+      return { status: res.status, value: res.reason.toString() };
+    }));
 }
