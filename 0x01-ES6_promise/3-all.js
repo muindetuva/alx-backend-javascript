e0aa2e6b268736f1d7df4df157da70aeac50eb54
Alfred Tuva <muindetuva@gmail.com>
2025-05-14 09:56:37 +0300

    chore: update gitignore

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,22 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  let body;
-  let firstName;
-  let lastName;
-
-  return uploadPhoto()
-    .then((data) => {
-      // Store body in a variable
-      body = data.body;
-
-      // Return a new promise
-      return createUser();
-    })
-    .then((data) => {
-      firstName = data.firstName;
-      lastName = data.lastName;
-      console.log(body, firstName, lastName);
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photoData, userData]) => {
+      console.log(photoData.body, userData.firstName, userData.lastName);
     })
     .catch(() => {
       console.log('Signup system offline');
