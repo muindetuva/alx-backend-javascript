1172c4e8b9cbd0a66447c2888c22fa94a3c658de
Alfred Tuva <muindetuva@gmail.com>
2025-05-14 20:43:38 +0300

    add task 2

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+export default function getStudentsByLocation(students, location) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  return students.filter((student) => student.location === location);
+}
