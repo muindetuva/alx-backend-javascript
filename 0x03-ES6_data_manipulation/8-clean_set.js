03063c2c7e1577fe5adc29c2a8a04a5287dd309d
Alfred Tuva <muindetuva@gmail.com>
2025-05-14 23:38:50 +0300

    Fix issues waah

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0 || typeof startString !== 'string') {
+  if (typeof startString !== 'string' || !startString) {
     return '';
   }
 
   const result = [];
 
   for (const item of set) {
-    if (item.startsWith(startString)) {
+    if (typeof item === 'string' && item.startsWith(startString)) {
       result.push(item.slice(startString.length));
     }
   }
