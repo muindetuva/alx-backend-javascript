13dc84f826b8149d6712f1c58900e5a2108ce395
Alfred Tuva <muindetuva@gmail.com>
2025-05-14 10:09:27 +0300

    Return one string with console

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photoData, userData]) => {
-      console.log(photoData.body, userData.firstName, userData.lastName);
+      console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
