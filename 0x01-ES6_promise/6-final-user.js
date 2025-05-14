17b37b259e150cc3f19bcf7bde6e41d97a3f53c7
Alfred Tuva <muindetuva@gmail.com>
2025-05-14 10:42:20 +0300

    Add task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,9 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      console.log(results);
+    });
+}
