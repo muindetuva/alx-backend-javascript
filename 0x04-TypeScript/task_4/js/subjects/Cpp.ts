61caad362be2d5276bc6d418c75c378a00138342
Alfred Tuva <muindetuva@gmail.com>
2025-05-15 16:12:43 +0300

    Add task 9

---
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,21 @@
+/// <reference path="Subject.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingC?: number; // Declaration merging
+  }
+
+  export class Cpp extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Cpp';
+    }
+
+    getAvailableTeacher(): string {
+      if (!this.teacher || this.teacher.experienceTeachingC === undefined || this.teacher.experienceTeachingC <= 0) {
+        return 'No available teacher';
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
+
---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,12 @@
+/// <reference path="Teacher.ts" />
+
+namespace Subjects {
+  export class Subject {
+    protected teacher: Teacher;
+
+    setTeacher(teacher: Teacher) {
+      this.teacher = teacher;
+    }
+  }
+}
+
