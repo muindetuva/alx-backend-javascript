14350871be98f6022e3ec26dbb43b2652d1b2f63
Alfred Tuva <muindetuva@gmail.com>
2025-05-15 21:53:56 +0300

    Test against test

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,22 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-let name = '';
-
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (chunk) => {
-  name += chunk;
-});
+let name = '';
 
-process.stdin.on('end', () => {
-  process.stdout.write(`Your name is: ${name}`);
-  process.stdout.write('This important software is now closing\n');
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (chunk) => {
+    name += chunk;
+    process.stdout.write(`Your name is: ${name.trim()}\n`);
+    process.exit(0); // End immediately after printing
+  });
+} else {
+  process.stdin.on('data', (chunk) => {
+    name += chunk;
+  });
+
+  process.stdin.on('end', () => {
+    process.stdout.write(`Your name is: ${name}`);
+    process.stdout.write('This important software is now closing\n');
+  });
+}
