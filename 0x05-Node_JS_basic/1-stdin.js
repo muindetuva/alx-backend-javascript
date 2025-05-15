151d2368ebb21a7e6b79a7dfdbff03da6f27b241
Alfred Tuva <muindetuva@gmail.com>
2025-05-15 21:43:56 +0300

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 process.stdout.write('Welcome to ALX, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write(`Your name is: ${data}\n`);
   if (process.stdin.isTTY) {
     process.exit();
   }
