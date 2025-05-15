d4726373884232850f00a242c749ef6eba84e14e
Alfred Tuva <muindetuva@gmail.com>
2025-05-15 21:58:17 +0300

    Last attempt at this

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,22 +1,20 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+// 1-stdin.js
+process.stdout.write('Welcome to ALX, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
 let name = '';
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (chunk) => {
-    name += chunk;
-    process.stdout.write(`Your name is: ${name.trim()}\n`);
-    process.exit(0); // End immediately after printing
-  });
-} else {
-  process.stdin.on('data', (chunk) => {
-    name += chunk;
-  });
+process.stdin.on('data', (chunk) => {
+  name += chunk;
 
-  process.stdin.on('end', () => {
+  if (process.stdin.isTTY) {
     process.stdout.write(`Your name is: ${name}`);
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+    process.exit(0);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write(`Your name is: ${name}`);
+  process.stdout.write('This important software is now closing\n');
+});
