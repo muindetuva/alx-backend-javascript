af8b553822188bf9bbdba5f6e47de64383021006
Alfred Tuva <muindetuva@gmail.com>
2025-05-15 21:37:10 +0300

    Add task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+process.stdout.write('Welcome to ALX, what is your name?\n');
+
+let name = '';
+
+process.stdin.on('data', (data) => {
+  name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.exit();
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
