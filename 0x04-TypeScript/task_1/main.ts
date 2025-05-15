cfa7490cc34757535c6ad2745882ddc4333fbb87
Alfred Tuva <muindetuva@gmail.com>
2025-05-15 15:45:25 +0300

    Add updates

--- 0x04-TypeScript/task_1/main.ts
+++ 0x04-TypeScript/task_1/main.ts
@@ -16,16 +16,37 @@ const teacher1: Teacher = {
   contract: true, // additional attribute
 };
 
-const teacher2: Teacher = {
-  firstName: 'John',
-  lastName: 'Kamau',
-  fullTimeEmployee: false,
-  yearsOfExperience: 5,
-  location: 'Mombasa',
-  isRemote: true,
-  subjects: ['Math', 'Physics'],
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
 };
 
-console.log(teacher1);
-console.log(teacher2);
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
 
