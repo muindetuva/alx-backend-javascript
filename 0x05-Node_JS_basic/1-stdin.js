6c7fe163e02caa47671461f3d729a19c45b808dc
Alfred Tuva <muindetuva@gmail.com>
2025-05-15 22:10:39 +0300

    Once more

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -10,6 +10,6 @@ process.stdin.on('readable', () => {
   }
 });
 
-process.stdin.on('exit', () => {
+process.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
 });
