6070eb2afe5d7547bd9b328c41dfcc81e4ba644b
Alfred Tuva <muindetuva@gmail.com>
2025-05-15 22:06:59 +0300

    Once more

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdin.setEncoding('utf8');
 
-process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
