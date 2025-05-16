3f03a8632fd2ec52364d54448228c020c03c745b
Alfred Tuva <muindetuva@gmail.com>
2025-05-16 21:03:47 +0300

    Add a fw more assertions

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -17,4 +17,19 @@ describe('calcualteNumber', function () {
   it('should return 6 when a=1.5 and b=3.7', function () {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
+
+  it('should round first number only', function () {
+    assert.strictEqual(calculateNumber(1.4, 3), 4); 
+    assert.strictEqual(calculateNumber(1.5, 3), 5);
+  });
+
+  it('should round second number only', function () {
+    assert.strictEqual(calculateNumber(1, 3.4), 4);
+    assert.strictEqual(calculateNumber(1, 3.5), 5);
+  });
+
+  it('should round both numbers', function () {
+    assert.strictEqual(calculateNumber(1.5, 3.5), 6);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
 });
