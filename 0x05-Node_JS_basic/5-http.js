84b6cd5d88bf656bc766ddf89303ae8eb393e72a
Alfred Tuva <muindetuva@gmail.com>
2025-05-16 08:21:11 +0300

    Update to holberton school

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -8,7 +8,7 @@ const app = http.createServer((req, res) => {
 
   if (req.url === '/') {
     res.statusCode = 200;
-    res.end('Hello ALX!');
+    res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     res.statusCode = 200;
     res.write('This is the list of our students\n');
